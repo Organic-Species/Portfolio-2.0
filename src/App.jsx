fa5d48ebae15108bff801b37bab955724be2afd1
Jarrel-Thomas <jarrel.thomas@gmail.com>
2022-10-21 22:11:47 -0400

    Added All Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import About from './components/about/about';
+import Contact from './components/contact/contact';
+import Experince from './components/exp/exp';
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
+import Nav from './components/nav/nav';
+import Portfolio from './components/portfolio/portfolio';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Nav />
+      <About />
+      <Experince />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
   );
 };
