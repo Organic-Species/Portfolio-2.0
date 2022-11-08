0dbe9522022d877332e42fa9025bfed20024a92f
Jarrel-Thomas <jarrel.thomas@gmail.com>
2022-11-07 22:38:49 -0500

    Experince & Portfolio Section. Mapped Portfolio from Array

--- src/components/exp/exp.jsx
+++ src/components/exp/exp.jsx
@@ -1,8 +1,86 @@
 import React from 'react';
+import { FaCheckCircle } from 'react-icons/fa';
 import './exp.css';
 
 const Experince = () => {
-  return <section id="experience">Experince</section>;
+  return (
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experince</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>Javascript</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>Wordpress</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>UI/UX Design</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        {/* END OF FRONTEND EXPERIENCE */}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>SQL</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckCircle className="experience__details-icon" />
+              <div>
+                <h4>Rest API</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experince;
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,8 +1,82 @@
 import React from 'react';
+import IMG2 from '../../assets/pexels-picjumbocom-196645 (1).jpg';
+import IMG3 from '../../assets/pexels-picjumbocom-196645 (2).jpg';
+import IMG4 from '../../assets/pexels-picjumbocom-196645 (3).jpg';
+import IMG5 from '../../assets/pexels-picjumbocom-196645 (4).jpg';
+import IMG1 from '../../assets/pexels-picjumbocom-196645.jpg';
 import './portfolio.css';
 
+// DO NOT USE IMAGES IN PRODUCTION
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Just another website',
+    github: 'https://github.com',
+    demo: 'http://loftart.epizy.com/',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Just another website',
+    github: 'https://github.com',
+    demo: 'http://loftart.epizy.com/',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Just another website',
+    github: 'https://github.com',
+    demo: 'http://loftart.epizy.com/',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Just another website',
+    github: 'https://github.com',
+    demo: 'http://loftart.epizy.com/',
+  },
+];
+
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
