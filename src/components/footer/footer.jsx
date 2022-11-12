0da4567b9a989751a7e442bdf5ddbafcc265bc3f
Jarrel-Thomas <jarrel.thomas@gmail.com>
2022-11-11 22:26:09 -0500

    Added Footer Section

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 import './footer.css';
 
 const footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        Jreal
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#exp">Expeirience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/jarrel-thomas/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Organic-Species">
+          <FaGithubSquare />
+        </a>
+        <a href="https://twitter.com/OrganicSpecies">
+          <FaTwitterSquare />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Jarrel Thomas. All Rights Reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default footer;
